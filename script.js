3f01aecda9cda43bb82b7fff31e4efa8c301424e
xyz19 <xyz1928374655@gmail.com>
2018-11-03 16:10:23 +0900

    JSによるタイトルクリックで詳細表示機能追加

---
+++ script.js
@@ -0,0 +1,50 @@
+
+$("body").fadeIn(700);
+
+
+
+
+var elem = document.getElementsByTagName("td");
+
+for(var i =0;i<elem.length;i++){
+  if(i%4==0){
+    elem[i].className ="modal-class";
+    elem[i].id = i;
+  }
+}
+
+var area = document.getElementsByClassName("modal-area");
+for(var i=0;i<area.length;i++){
+  area[i].id = "modal"+i*4;
+}
+
+// $(".modal-class").each(function(){
+//   $(this).on("click",function(event){
+//
+//     $("#modal"+$(this).attr("id")).iziModal();
+//
+//     console.log($('#modal'+$(this).attr("id")));
+//   })
+// });
+
+var test =document.getElementsByClassName("modal-class");
+var c = test[0].textContent.slice(1);
+
+
+$(".modal-class").each(function(){
+  //jqueryだとうまくできないからjsでやる
+  title = document.getElementsByClassName("modal-class")[$(this).attr("id")/4].textContent.slice(1);
+  //モーダルを設定 addするイメージ
+  $("#modal"+$(this).attr("id")).iziModal(
+    {
+      title:title,
+      headerColor: '#005ab3', //ヘッダー部分の色
+      overlayColor: 'rgba(0, 0, 0, 0.5)', //モーダルの背景色
+    }
+  );
+  $(this).on("click",function(event){
+    //クリックしたモーダルを表示
+      $("#modal"+$(this).attr("id")).iziModal("open");
+    console.log($(this).contents()[1]);
+  })
+});
--- tablesort.js
+++ tablesort.js
@@ -1,45 +1,26 @@
-/*
-Table Sorter v2.1
-Adds bi-directional sorting to table columns.
-Copyright (C) 2005 Digital Routes, Scotland
-Copyright (C) 2007 Neil Fraser, California
-http://neil.fraser.name/software/tablesort/
-
-This program is free software you can redistribute it and/or
-modify it under the terms of the GNU Lesser General Public
-License as published by the Free Software Foundation.
-
-This program is distributed in the hope that it will be useful,
-but WITHOUT ANY WARRANTY without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-GNU Lesser General Public License (www.gnu.org) for more details.
-
-Include on your page:
-  <SCRIPT LANGUAGE='JavaScript1.2' SRC='tablesort.js'></SCRIPT>
-*/
 
 
-// Namespace object.
+
+
+
 var TableSort = {};
 
-// Default text values for the arrows.  Override these with custom image tags.
+
 TableSort.arrowNone = " &nbsp;";
 TableSort.arrowUp   = " &uarr;";
 TableSort.arrowDown = " &darr;";
 
-// List of all the tables
+
 TableSort.tables = [];
 
-// Upon which column was the table sorted last time.  -=up, +=down
+
 TableSort.lastSort = [];
 
 
-// When the page finishes loading, add initialise all tables.
-// Preserve any existing onload function.
 if (window.onload) {
   TableSort.oldOnload = window.onload;
 }
-// Set a new onload function.
+
 window.onload = function() {
   TableSort.init();
   if (TableSort.oldOnload) {
@@ -48,24 +29,18 @@ window.onload = function() {
 }
 
 
-// Make one, several or all tables sortable.
-// Call this function with the ID(s) of any tables which are created
-// with DTHML after the page has loaded.
 TableSort.init = function() {
   if (navigator.appName == "Microsoft Internet Explorer" &&
       navigator.platform.indexOf("Mac") == 0) {
-    // The Mac version of MSIE is way too buggy to deal with.
     return;
   }
   if (arguments.length == 0) {
-    // Locate all the document's tables.
     var tableNodeList = document.getElementsByTagName('TABLE');
     for (var x = 0; x < tableNodeList.length; x++) {
       TableSort.tables.push(tableNodeList[x]);
       TableSort.initTable(x);
     }
   } else {
-    // Initialise only the specified tables.
     var table;
     for (var x = 0; x < arguments.length; x++) {
       table = document.getElementById(arguments[x]);
@@ -78,7 +53,7 @@ TableSort.init = function() {
 }
 
 
-// Turn all the header/footer cells of one table into sorting links.
+
 TableSort.initTable = function(t) {
   var table = TableSort.tables[t];
   if (table.tHead) {
@@ -99,7 +74,6 @@ TableSort.initTable = function(t) {
 }
 
 
-// Turn one header/footer cell into a sorting link.
 TableSort.linkCell = function(cell, t, x, y) {
   if (TableSort.getLabel(cell)) {
     var link = document.createElement('A');
@@ -111,7 +85,6 @@ TableSort.linkCell = function(cell, t, x, y) {
     for (var c = 0; c < cell.childNodes.length - 1; c++) {
       link.appendChild(cell.childNodes[c]);
     }
-    // Add an element where the sorting arrows will go.
     var arrow = document.createElement('SPAN');
     arrow.innerHTML = TableSort.arrowNone;
     arrow.name = "TableSort_" + t + "_" + x + "_" + y;
@@ -120,14 +93,10 @@ TableSort.linkCell = function(cell, t, x, y) {
 }
 
 
-// Return the 'label' attribute for a cell.
-// Opera won't let us make up novel attribute names,
-// so 'label' is picked because it is obscure.
+
 TableSort.getLabel = function(cell) {
   var str;
   if (window.opera) {
-    // Opera 7 & 8 have a bug with getAttribute,
-    // so this is an ugly hack to sidestep it.
     var m = cell.outerHTML.match(/^<[^>]+LABEL=['"]*([^'" ]+)['"]*/i);
     str = m ? m[1] : "";
   } else {
@@ -137,7 +106,6 @@ TableSort.getLabel = function(cell) {
 }
 
 
-// Sort the rows in this table by the specified column.
 TableSort.click = function(table, column, mode) {
   if (!mode.match(/^[_a-z0-9]+$/)) {
     alert("Illegal sorting mode type.");
@@ -148,7 +116,6 @@ TableSort.click = function(table, column, mode) {
     alert("Unknown sorting mode: " + mode);
     return;
   }
-  // Determine and record the direction.
   var reverse = false;
   if (Math.abs(TableSort.lastSort[table]) == column + 1) {
     reverse = TableSort.lastSort[table] > 0;
@@ -156,7 +123,6 @@ TableSort.click = function(table, column, mode) {
   } else {
     TableSort.lastSort[table] = column+1;
   }
-  // Display the correct arrows on every header/footer cell.
   var spans = document.getElementsByTagName('SPAN');
   var spanprefix1 = "TableSort_" + table + "_";
   var spanprefix2 = "TableSort_" + table + "_" + column;
@@ -174,9 +140,8 @@ TableSort.click = function(table, column, mode) {
       }
     }
   }
-  // Fetch the table's data and store it in a dictionary (assoc array).
   if (TableSort.tables[table].tBodies.length < 1) {
-    return; // No data in table.
+    return;
   }
   var tablebody = TableSort.tables[table].tBodies[0];
   var cellDictionary = [];
@@ -184,23 +149,23 @@ TableSort.click = function(table, column, mode) {
   for (var y = 0; y < tablebody.rows.length; y++) {
     if (tablebody.rows[y].cells.length > 0) {
       cell = tablebody.rows[y].cells[column];
-    } else { // Dodge Safari 1.0.3 bug
+    } else {
       cell = tablebody.rows[y].childNodes[column];
     }
     cellDictionary[y] = [TableSort.dom2txt(cell), tablebody.rows[y]];
   }
-  // Sort the dictionary.
+
   cellDictionary.sort(compareFunction);
-  // Rebuild the table with the new order.
+
   var i;
   for (y = 0; y < cellDictionary.length; y++) {
     i = reverse ? (cellDictionary.length - 1 - y) : y;
     tablebody.appendChild(cellDictionary[i][1]);
   }
   if (window.opera) {
-    // Opera needs to rerender the last row due to a redraw bug.
+
     setTimeout(function() {
-      // This is a closure.
+
       tablebody.appendChild(tablebody.removeChild(
           tablebody.rows[tablebody.rows.length - 1]));
     }, 1);
@@ -208,8 +173,6 @@ TableSort.click = function(table, column, mode) {
 }
 
 
-// Recursively build a plain-text version of a DOM structure.
-// Bug: whitespace isn't always correct, but shouldn't matter for tablesort.
 TableSort.dom2txt = function(obj) {
   var text = "";
   if (!obj) {
@@ -226,8 +189,6 @@ TableSort.dom2txt = function(obj) {
 }
 
 
-// Case-sensitive sorting.
-// Compare two dictionary structures and indicate which is larger.
 TableSort.compare_case = function(a, b) {
   if (a[0] == b[0]) {
     return 0;
@@ -236,8 +197,6 @@ TableSort.compare_case = function(a, b) {
 }
 
 
-// Case-insensitive sorting.
-// Compare two dictionary structures and indicate which is larger.
 TableSort.compare_nocase = function(a, b) {
   var aLower = a[0].toLowerCase();
   var bLower = b[0].toLowerCase();
@@ -248,8 +207,6 @@ TableSort.compare_nocase = function(a, b) {
 }
 
 
-// Numeric sorting.
-// Compare two dictionary structures and indicate which is larger.
 TableSort.compare_num = function(a, b) {
   var aNum = parseFloat(a[0]);
   if (isNaN(aNum)) {
